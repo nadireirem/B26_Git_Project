f32930b09952ceb00846d2be7a1f0ad71e5ecf19
nadireirem <nadireulsular@gmail.com>
2022-03-26 18:33:48 -0400

    Added first class and changed the note

--- src/day0/First.java
+++ src/day0/First.java
@@ -1,7 +1,7 @@
 package day0;
 
 public class First {
-    public static void main(String[] args) {
-        System.out.println("second file");
-    }
+    //TODO: Work on this tomorrow add some cool code
+
+
 }
